c9164292715bc38c3ec15db3c75ce507492646ec
thtv231 <126608842+thtv231@users.noreply.github.com>
2024-06-15 11:28:03 +0700

    tạo collection account

---
+++ model/acounts.model.js
@@ -0,0 +1,26 @@
+
+const mongoose = require("mongoose")
+const generate = require("../helpers/generate")
+mongoose.plugin(slug)
+const accountSchema = new mongoose.Schema({
+    fullName: String,
+    email: String,
+    password: String,
+   
+    token : {
+       type: String,
+       default: generate.generateRandomString(20) 
+    },
+    phone : String,
+    avatar: String,
+    status: String,
+    deleted: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt:Date
+},{timestamps:true})
+
+const Account = mongoose.model("Account",accountSchema,"accounts")
+
+module.exports = Account
