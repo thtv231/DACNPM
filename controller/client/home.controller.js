4c9ae5814a6cd181b9352b469967a32f193a074c
thtv231 <126608842+thtv231@users.noreply.github.com>
2024-06-21 19:02:38 +0700

    thêm sản phẩm nổi bật

--- controller/client/home.controller.js
+++ controller/client/home.controller.js
@@ -1,8 +1,26 @@
-
+const Products = require("../../model/products.model")
 
 module.exports.index = async (req, res) => {
+    
+   
+    
+    const find = {
+        deleted: false,
+        status: "active",
+        featured:"1"
+    };
 
-    res.render("client/pages/home/index"
-        
-    ) 
+   
+    const products = await Products.find(find)
+   
+    products.forEach(item => {
+        item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed(0)
+    })
+    //console.log(products)
+    res.render("client/pages/home/index",
+        {
+            products: products,
+           
+        }
+    )
 }
