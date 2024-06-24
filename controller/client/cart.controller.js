1fb4f85c9e85216bb58659aa2584f74f87436649
thtv231 <126608842+thtv231@users.noreply.github.com>
2024-06-24 12:26:12 +0700

    thêm trang giỏ hàng

--- controller/client/cart.controller.js
+++ controller/client/cart.controller.js
@@ -1,5 +1,41 @@
 const Cart = require("../../model/cart.model")
+const Product = require("../../model/products.model")
+const productHelper = require("../../helpers/products")
 
+
+
+module.exports.index =async (req,res)=>{
+    const cartId = req.cookies.cartId
+    const cart= await Cart.findOne({
+        _id: cartId
+    })
+
+    if(cart.products.length >0){
+        let total =0
+        for(const item of cart.products){
+            const productId = item.product_id
+            const productInfo = await Product.findOne({
+                _id:productId
+            })
+
+            productInfo.priceNew = productHelper.priceNewProduct(productInfo)
+            item.productInfo = productInfo
+            total+=parseInt(productInfo.priceNew)*parseInt(item.quantity)
+        }
+        cart.totalPrice = total
+        
+    }
+
+
+    //console.log(cart)
+    res.render("client/pages/cart/index",
+        {
+            pageTitle: "Giỏ Hàng",
+            cartDetail : cart
+           
+        }
+    )
+}
 
 module.exports.addPost =async (req,res)=>{
     const cartId = req.cookies.cartId
--- router/client/cart.router.js
+++ router/client/cart.router.js
@@ -2,6 +2,8 @@ const express = require("express")
 const router = express.Router()
 const controller = require("../../controller/client/cart.controller.js")
 
+router.get("/",controller.index)
+
 router.post('/add/:productId',controller.addPost )
 
 
